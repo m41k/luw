bf5edb4024cbce523cd9db6cd5ea1ccba57701f2
m41k <maik.alberto@hotmail.com>
2016-06-02 21:14:25 -0300

    Add ATTACH and CONSOLE
    teste - arrumar codigo

--- luw.sh
+++ luw.sh
@@ -5,6 +5,7 @@
 #--------------------------------------------------------------------------------#
 
 eval `/opt/luw/proccgi $*`
+
 echo -e "Content-type: text/html\n\n"
 
 #--------------------------------------------------------------------------------#
@@ -30,6 +31,50 @@ if [ $puser != $REMOTE_USER ];
 fi 2> /dev/null
 
 #--------------------------------------------------------------------------------#
+#  			        BUILD CONSOLE			                 #
+#--------------------------------------------------------------------------------#
+function Fss()
+{
+cpid=~/.pid_$container.lxc
+port=`echo $[ 10000 + $[ RANDOM % 10000 ]]`
+echo $port
+netcat -z 127.0.0.1 $port
+if [ $? = 0 ]; then
+ Fss
+else
+# shellinaboxd --no-beep -p $port -t -b$cpid -s "/:AUTH:HOME:lxc-attach -n $container"
+  shellinaboxd --no-beep -p $port -t -b$cpid -s "/:AUTH:HOME:/bin/bash /home/ubuntu/public_html/cgi-bin/apoio"
+fi
+
+
+#echo "shellinaboxd --no-beep -p $port -t -bteste.pid -s '/:AUTH:HOME:lxc-attach -n $container'"
+}
+
+function Fsp()
+{
+cpid=~/.pid_$container.lxc
+pidc=`cat $cpid`; kill -9 $pidc
+}
+
+function Fst()
+{
+(
+#./luw.sh -teste
+echo "#!/bin/bash" > $container.sh
+echo shellinaboxd --no-beep --cgi -t -s '"'/:$(id -u):$(id -g):HOME:/bin/bash /home/ubuntu/public_html/cgi-bin/$container.box'"' >> $container.sh
+echo lxc-console -q -n $container > $container.box
+chmod 705 $container.sh
+chmod +x $container.sh
+goshell='<meta http-equiv="refresh" content="0;url=http://'$SERVER_NAME'/~'$REMOTE_USER'/cgi-bin/'$container.sh'">'
+echo $goshell
+
+#echo teste
+)
+#echo $0
+#shellinaboxd --no-beep --cgi -t -s "/:$(id -u):$(id -g):HOME:/bin/bash /home/ubuntu/public_html/cgi-bin/apoio"
+}
+
+#--------------------------------------------------------------------------------#
 #	CATALOGO DE COMANDOS - INDICE:NOMEBOTAO:COMANDO - CRIACAO ARQUIVO	 #
 #--------------------------------------------------------------------------------#
 catcom=~/.catcom.lxc
@@ -39,6 +84,41 @@ eval $ssh echo "3:stop:$ssh lxc-stop -n" >> $catcom 2> /dev/null
 eval $ssh echo "4:freeze:$ssh lxc-freeze -n" >> $catcom 2> /dev/null
 eval $ssh echo "5:unfreeze:$ssh lxc-unfreeze -n" >> $catcom 2> /dev/null
 eval $ssh echo "6:destroy:$ssh lxc-destroy -n" >> $catcom 2> /dev/null
+eval $ssh echo "7:console:Fst" >> $catcom 2> /dev/null
+
+#--------------------------------------------------------------------------------#
+#		BOTOES CONTAINERS  - FILEIRA DE BOTOES - EXECUCAO		 #
+#--------------------------------------------------------------------------------#
+#=======================EXECUTANDO COMANDO DO BOTAO CLICADO======================#
+if [ $FORM_botao != "" ]; then
+	ivn=`echo $FORM_botao | awk -F: {'print $1'}` 
+	echo "<pre>"
+	comand=`cat -n $catcom | grep $ivn | awk -F: {'print $3'}`
+	container=`echo $FORM_botao | awk -F: {'print $2'}`
+	var2=`echo $comand $container`
+	eval $var2
+fi 2> /dev/null
+
+#--------------------------------------------------------------------------------#
+#		    CRIANDO LISTA DE CONTAINERS EXISTENTES			 #
+#--------------------------------------------------------------------------------#
+
+lslxc=~/.contdo$REMOTE_USER.lxc
+eval $ssh lxc-ls -f > $lslxc 2> /dev/null
+
+#--------------------------------------------------------------------------------#
+#			HTML - BOTOES CABECALHO	- CRIACAO			 #
+#--------------------------------------------------------------------------------#
+echo "<form method='post' action='$luw'>"
+btop_home="<input type='submit' name='top'  value='Home'>"
+btop_chek="<input type='submit' name='top'  value='Check Config'>"
+btop_ncon="<input type='submit' name='top' value='New Container'>"
+btop_clon="<input type='submit' name='top' value='Clone'>"
+btop_atta="<input type='submit' name='top' value='Attach'>"
+echo $btop_home $btop_chek $btop_ncon $btop_clon $btop_atta
+#echo "<input type='submit' name='top'  value='Home'>" "<input type='submit' name='top'  value='Check Config'>" "<input type='submit' name='top' value='New Container'>" "<input type='submit' name='top' value='Clone'>"
+echo "</form>"
+
 
 #--------------------------------------------------------------------------------#
 #			BOTOES CABECALHO - EXECUCAO				 #
@@ -51,9 +131,9 @@ fi 2> /dev/null
 
 #============================[BOTAO CHECKCONFIG]=================================#
 if [ $FORM_top = "Check Config" ]; then 
-	echo "<form method='post' action='$luw'>"
-	echo "<input type='submit' name='top'  value='Home'>" 
-	echo "</form>"
+#	echo "<form method='post' action='$luw'>"
+#	echo "<input type='submit' name='top'  value='Home'>" 
+#	echo "</form>"
 	echo "<pre>"
 	echo "<h2>LXC-CHECKCONFIG</h1>"
 	lxc-checkconfig | sed 's/\[0;39m/ / ; s/\[1;32m/ /'
@@ -94,17 +174,70 @@ if [ $FORM_top = "New Container" ]; then
 	echo  "</form>"
 fi 2> /dev/null
 
-#--------------------------------------------------------------------------------#
-#		BOTOES CONTAINERS  - FILEIRA DE BOTOES - EXECUCAO		 #
-#--------------------------------------------------------------------------------#
-#=======================EXECUTANDO COMANDO DO BOTAO CLICADO======================#
-if [ $FORM_botao != "" ]; then
-	ivn=`echo $FORM_botao | awk -F: {'print $1'}` 
-	echo "<pre>"
-	comand=`cat -n $catcom | grep $ivn | awk -F: {'print $3'}`
-	container=`echo $FORM_botao | awk -F: {'print $2'}`
-	var2=`echo $comand $container`
-	eval $var2
+#=================================[BOTAO CLONE]==================================#
+if [ $FORM_top = "Clone" ]; then 
+#------->Criar array do containers existentes para o select 
+	wc_lslxc=`wc $lslxc | awk {'print $1'}`
+	tt_cont=`expr $wc_lslxc - 1`
+	orig=( `cat $lslxc | awk {'print $1'} | tail -$tt_cont` )
+
+#------->Form para criacao de container
+
+	echo "<form method='post' action='$luw'>"
+	echo "<h2>Clone Container</h2>"
+	echo "<select name='orig'>"
+         for (( d=1; d<=${#orig[@]}; d++ ))
+          do
+ 	   echo "<option value=$orig[$d]>$orig[$d]"
+         done
+	echo  "</select>"
+	   echo ">>>"
+	   echo "<input type='text' name='clone' maxlength='50' size='30'>"
+  	   echo  "<input type='submit' value='Clonar'>"
+	echo  "</form>"
+
+#	echo "</pre>"
+fi 2> /dev/null
+
+#=================================[BOTAO ATTACH]==================================#
+if [ $FORM_top = "Attach" ]; then
+#------->Criar array do containers existentes para o select
+        wc_lslxc=`wc $lslxc | awk {'print $1'}`
+        tt_cont=`expr $wc_lslxc - 1`
+        orig=( `cat $lslxc | awk {'print $1'} | tail -$tt_cont` )
+
+#------->Form para criacao de container
+
+        echo "<form method='post' action='$luw'>"
+#        echo "<h2>Attach Command</h2>"
+        echo "<h2>Define password:</h2>"
+#        echo "root@"
+         echo "Container:"
+        echo "<select name='orig'>"
+         for (( d=1; d<=${#orig[@]}; d++ ))
+          do
+           echo "<option value=$orig[$d]>$orig[$d]"
+         done
+        echo  "</select>"
+#          echo ":/#"
+#           echo "<input type='text' name='attach' maxlength='50' size='30'>"
+#           echo  "<input type='submit' value='Execute'>"
+	echo "User:"
+        echo "<select name='user'>"
+	echo "<option value='luw'>luw</option>"
+	echo "<option value='root'>root</option>"
+        echo  "</select>"
+	echo "Password:"
+        echo "<input type='password' name='pass' maxlength='50' size='30'>"
+        echo  "<input type='submit' value='Execute'>"
+
+#	   echo "<br>"
+#           echo "<input type='text' name='aroot' maxlength='50' size='30'>"
+#           echo  "<input type='submit' value='Execute'>"
+
+        echo  "</form>"
+
+#       echo "</pre>"
 fi 2> /dev/null
 
 #--------------------------------------------------------------------------------#
@@ -127,18 +260,37 @@ if [ $FORM_ncont != "" ]; then
 fi 2> /dev/null
 
 #--------------------------------------------------------------------------------#
-#			HTML - BOTOES CABECALHO	- CRIACAO			 #
+#                       BOTAO CLONE CONTAINER - EXECUCAO                         #
 #--------------------------------------------------------------------------------#
-echo "<form method='post' action='$luw'>"
-echo "<input type='submit' name='top'  value='Home'>" "<input type='submit' name='top'  value='Check Config'>" "<input type='submit' name='top' value='New Container'>"
-echo "</form>"
+#==================================LXC-CLONE=====================================#
+if [ $FORM_clone != "" ]; then
+ if  echo $FORM_clone | grep '[^[:alnum:]]' > /dev/null; then
+  echo "<font color=red size=2><b>Invalid name. Use alphanumeric characters only.</b></font>"
+ else
+        echo "<pre>"
+#        eval $ssh lxc-create -t download -n $contname -- -d $distro -r $release -a $arquit 2> /dev/null
+	 eval $ssh  lxc-clone -o $FORM_orig -n $FORM_clone 2> /dev/null 
+        echo "</pre>"
+ fi
+fi 2> /dev/null
 
 #--------------------------------------------------------------------------------#
-#		    CRIANDO LISTA DE CONTAINERS EXISTENTES			 #
+#                       BOTAO ATTACH COMMAND - EXECUCAO                          #
 #--------------------------------------------------------------------------------#
+#==================================LXC-ATTACH====================================#
+if [ $FORM_pass != "" ]; then
+        echo "<pre>"
+          eval $ssh lxc-start -n $FORM_orig 2> /dev/null 
+          eval $ssh lxc-attach -n $FORM_orig -- useradd -m $FORM_user -s /bin/bash
+          eval $ssh lxc-attach -n $FORM_orig -- usermod -p $(openssl passwd $FORM_pass) $FORM_user
+        echo "Password changed. <a href=http://$SERVER_NAME/~$REMOTE_USER/cgi-bin/$FORM_orig.sh>Have Fun!</a>"
+        echo "</pre>"
 
 lslxc=~/.contdo$REMOTE_USER.lxc
 eval $ssh lxc-ls -f > $lslxc 2> /dev/null
+
+fi 2> /dev/null
+
 
 #--------------------------------------------------------------------------------#
 #			    VARIAVES AUXILIARES SHELL				 #
@@ -201,7 +353,7 @@ echo "<form method='post' action='$luw'>"
 	echo	  "<td bgcolor=red><pre>"
         	 ;;
         	 RUNNING)
-	echo 	  "<td bgcolor=green><pre>"
+	echo	  "<td bgcolor=green><pre>"
     		 ;;
     		 FROZEN)
 	echo 	  "<td bgcolor=blue><pre>"
