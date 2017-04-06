d13fe37c7c65f6427e5f9214532ddd60109314dc
m41k <maik.alberto@hotmail.com>
2017-04-06 09:09:42 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#Install - Ubuntu 16.04  
+
+ #Atualizando
+  apt-get update
+
+#Instalacoes
+ 
+ #Instalando LXC
+  apt-get install lxc -y
+
+ #Instalando Apache2
+  apt-get install apache2 -y
+
+ #Instalando Modulos Apache2
+  apt-get install libapache2-mod-authnz-external apache2-suexec-custom -y
+
+ #Instalando pacotes necessários
+ #apt-get install gcc -y
+ apt-get install zsh -y
+ apt-get install lynx -y
+ apt-get install shellinabox -y
+
+
+ #Habilitando Modulos
+  a2enmod userdir cgi authnz_external suexec
+
+ #Configurando quantidades de interfaces lxc
+  echo "ubuntu veth lxcbr0 10" >> /etc/lxc/lxc-usernet
+
+
+ #Configurando ambiente web
+
+ #Conf apache
+   cd /etc/apache2/sites-available/
+   mv 000-default.conf 000-default.bkp
+   wget https://raw.githubusercontent.com/m41k/luw/master/etc/apache2/sites-enabled/000-default.conf
+ 
+
+ #Preparando index
+   cd /var/www/html/
+   mv index.html index.bkp
+   wget https://raw.githubusercontent.com/m41k/luw/master/var/www/html/capa.png
+   wget https://raw.githubusercontent.com/m41k/luw/master/var/www/html/capa.png
+   wget https://raw.githubusercontent.com/m41k/luw/master/var/www/html/index.html
+
+ #Preparando cgi-bin
+
+  cd /usr/lib/cgi-bin
+  wget  https://raw.githubusercontent.com/m41k/luw/master/usr/lib/cgi-bin/luw-enter/luw-enter.sh
+  wget https://raw.githubusercontent.com/m41k/luw/master/usr/lib/cgi-bin/luw-ini.sh
+  wget https://raw.githubusercontent.com/m41k/luw/master/usr/lib/cgi-bin/luw-tuser.sh 
+  chmod +x *
+
+ #Preparando cgi-bin/luw-enter
+
+  mkdir /usr/lib/cgi-bin/luw-enter
+  cd /usr/lib/cgi-bin/luw-enter
+  wget https://raw.githubusercontent.com/m41k/luw/master/usr/lib/cgi-bin/luw-enter/luw-enter.sh
+  chmod +x *
+
+ #Preparando /opt/luw
+   mkdir /opt/luw
+   cd /opt/luw
+   wget https://github.com/m41k/luw/raw/master/opt/luw/proccgi
+   wget https://raw.githubusercontent.com/m41k/luw/master/opt/luw/luw.sh
+   chmod +x *
+
+ #Editando /etc/sudoers
+   echo "www-data ALL=NOPASSWD:/usr/lib/cgi-bin/luw-tuser.sh" >> /etc/sudoers
+   
+
+ #Reiniciando Apache
+  /etc/init.d/apache2 restart
