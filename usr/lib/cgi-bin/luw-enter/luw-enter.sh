4f567ca0940918d96ed36833e7a53e8c1b2ab98e
m41k <maik.alberto@hotmail.com>
2017-05-17 19:08:20 -0300

    Update luw-enter.sh

--- usr/lib/cgi-bin/luw-enter/luw-enter.sh
+++ usr/lib/cgi-bin/luw-enter/luw-enter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rodared='<meta http-equiv="refresh" content="0;url=http://'$SERVER_NAME'/~'$REMOTE_USER'/cgi-bin/luw.sh">'
+rodared='<meta http-equiv="refresh" content="0;url=http://'$SERVER_NAME:$SERVER_PORT'/~'$REMOTE_USER'/cgi-bin/luw.sh">'
 echo "Content-type: text/html"
 echo ""
 echo $rodared
