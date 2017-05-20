38ef60c66d9fc998ca89f1197cc11a668ba755a0
m41k <maik.alberto@hotmail.com>
2017-05-19 23:17:01 -0300

    Create luw-fw.sh

---
+++ opt/luw/tools/luw-fw.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------------------#
+#       LUW-FW - MODULO PARA CONFIGURACAO DE PORTAS PARA USUARIOS                #
+#               CREATED BY: maik.alberto@hotmail.com                             #
+#--------------------------------------------------------------------------------#
+
+ARQPORT=portas.fw
+
+case $1 in
+
+#->[c] = Criar lista de portas verificando portas em uso
+   -c)
+        #Definicao de rande de porta (mudar para var)
+        PI=$2
+        PF=$3
+
+        rm -f $ARQPORT
+
+        for ((i=$PI;i<=$PF;i++)); do
+         printf "$i:" >> $ARQPORT
+         fuser -sn tcp $i
+         if [ $? = 0 ]; then
+          printf "uso\n" >> $ARQPORT
+         else
+          printf "\n" >> $ARQPORT
+         fi
+        done
+;;
+#->[u] = Definir porta para usuario
+   -u)
+        USER=$2
+        LAS=`grep ':$' $ARQPORT | head -1`
+        #echo $LAS
+        sed -i s/$LAS/$LAS$USER/g $ARQPORT 2> /dev/null
+;;
+
+esac
