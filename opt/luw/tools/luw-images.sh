5cfc5a1a4c648383cd1416da343e9cc8f5b608f7
m41k <maik.alberto@hotmail.com>
2017-07-21 01:45:35 -0300

    Add files via upload

--- opt/luw/tools/luw-images.sh
+++ opt/luw/tools/luw-images.sh
@@ -5,6 +5,8 @@
 #--------------------------------------------------------------------------------#
 eval `/opt/luw/proccgi $*`
 
+echo -e "Content-type: text/html\n\n"
+
 #CONFIGURACAO
 URL="https://us.images.linuxcontainers.org"
 RLOCAL="/tmp/repo"
@@ -14,64 +16,120 @@ ARQ2="rootfs.tar.xz"
 BUIL="build_id"
 META="/meta/1.0"
 INDX="/index-user"
-
-#VERICACAO EXISTENCIA CARDAPIO
-[ -e $RLOCAL$CARD ] || > $RLOCAL$CARD
-
-#LISTA PARA DOWNLOAD
-LISTA=`wget -qO- https://us.images.linuxcontainers.org/meta/1.0/index-user`
-#DIFERENCA ENTRE REPOSITORIO LOCAL E DISPONIVEIS PARA DOWNLOAD
-#diff cardapio.luw index-user | grep ">" | tr -d ">" | tail -n +2
-DISP=`diff $RLOCAL$CARD $RLOCAL$INDX | grep ">" | tr -d ">"`
+F5='<meta http-equiv="refresh" content="0;">'
 
 #ATAULIZADO LISTA DISPONIVEL PARA DOWNLOAD
 rm -rf $RLOCAL$INDX
 wget $URL$META$INDX -P $RLOCAL
 
-echo -e "Content-type: text/html\n\n"
-echo "<pre>"
-echo "Distros Repositorio Local"
-echo "<hr>"
-cat $RLOCAL$CARD
-echo "<hr>"
-echo "Distros Disponives para download"
-echo "<form method='post' action='$proc'>"
+#VERICACAO EXISTENCIA CARDAPIO && ORDENA OU CRIA
+#[ -e $RLOCAL$CARD ] || > $RLOCAL$CARD
+[ -e $RLOCAL$CARD ] && sort $RLOCAL$CARD -o $RLOCAL$CARD || > $RLOCAL$CARD
+#[ -e lista ] && sort lista -o lista || > lista
 
+#DIFERENCA ENTRE REPOSITORIO LOCAL E DISPONIVEIS PARA DOWNLOAD
+#diff cardapio.luw index-user | grep ">" | tr -d ">" | tail -n +2
+DISP=`diff $RLOCAL$CARD $RLOCAL$INDX | grep ">" | tr -d ">"`
+#DISP=`diff $RLOCAL$CARD $RLOCAL$INDX | grep "[<>]" | tr -d "<>"`
+#LISTA DOS BAIXADOS
+BAIX=`cat $RLOCAL$CARD`
+#BAIX=`sort $RLOCAL$CARD > $RLOCAL/scard`
+#BAIX2=`cat $RLOCAL/scard`
 
-NUM=1
-        for line in $DISP
-        do
-           #echo $NUM "<input type='checkbox' name='list$NUM' value='$line'>" $line
-           echo "<input type='checkbox' name='list$NUM' value='$line'>" $line
-           NUM=`expr $NUM + 1`
-        done
+#--------------------------------------------------------------------------------#
+#                              CARPADIO LOCAL                                    #
+#--------------------------------------------------------------------------------#
 
-echo "<hr>"
-echo "<input type='checkbox' name='confirma' value='on'>" Desejo baixar "<input type='submit' name='ok' value='ok'>"
-echo "<hr>"
-echo "</form>"
+printf "<pre>"
+printf "<b><u>Distros Repositorio Local</u></b>"
+printf "<hr>"
+printf "<form method='post' action='$proc'>"
 
- if [ $FORM_confirma = "on" ]; then
+	LNUM=1
+	for LLOCAL in $BAIX
+	do
+#         echo "<input type='checkbox' name='LLOCAL$NUM' value='$LLOCAL'>" $LLOCAL
+	  ALLOCAL[$LNUM]=$LLOCAL
+#         echo $LNUM "<input type='submit' name='del$LNUM' value='Del'>" $LLOCAL
+          echo "<input type='submit' name='del$LNUM' value='Del'>" $LLOCAL
+
+	   LNUM=`expr $LNUM + 1`
+	done
+
+printf "<br>"
+
+#--------------------------------------------------------------------------------#
+#                              CARPADIO REMOTO                                    #
+#--------------------------------------------------------------------------------#
+printf "<b><u>Distros Disponiveis para Download</u></b>"
+#echo "Distros Disponives para download"
+printf "<hr>"
+
+	NUM=1
+	for line in $DISP
+	do
+#	   echo $NUM "<input type='checkbox' name='list$NUM' value='$line'>" $line
+	   echo "<input type='checkbox' name='list$NUM' value='$line'>" $line
+	   NUM=`expr $NUM + 1`
+	done
+
+#echo "<hr>"
+#echo "<input type='checkbox' name='confirma' value='on'>" Desejo baixar "<input type='submit' name='ok' value='ok'>"
+printf "<hr>"
+echo "<input type='submit' name='download' value='download'>"
+printf "</form>"
+
+#--------------------------------------------------------------------------------#
+#                       BOTAO DEL - APAGA DISTRO LOCAL                           #
+#--------------------------------------------------------------------------------#
+
+    for t in $(seq $LNUM);
+      do
+ 	  FORM1='$FORM_del'
+	  CFOR1=$FORM1$t
+	  FNUM1=`eval echo $CFOR1`
+         if [[ $FNUM1 != "" ]]; then
+#APAGANDO NO CARDAPIO
+         sed -i $(echo $t)d $RLOCAL$CARD
+#APAGANDO NO REPOSITORIO
+	  DIR=`echo ${ALLOCAL[t]} | awk -F";" {'print $6'}`
+          BID=`echo $DIR  | awk -F"/" {'print $7'}`
+          DIR=`echo $DIR  | sed "s/$BID\///"`
+	  rm -rf $RLOCAL$DIR
+#TESTE EXIBICAO
+#          echo ${ALLOCAL[t]}
+#REFRESH PAGE
+           echo $F5
+         fi
+      done
+
+#--------------------------------------------------------------------------------#
+#                       BOTAO DOWNLOAD - BAIXAR DISTRO                           #
+#--------------------------------------------------------------------------------#
+
+# if [ $FORM_confirma = "on" ]; then
+ if [ $FORM_download = "download" ]; then
     for l in $(seq $NUM);
       do
 
-          FORM='$FORM_list'
-          CFOR=$FORM$l
-          FNUM=`eval echo $CFOR`
+ 	  FORM='$FORM_list'
+	  CFOR=$FORM$l
+	  FNUM=`eval echo $CFOR`
 
-          if [[ $FNUM != "" ]]; then
-           #DIR=`echo $FNUM | cut -d ";" -f6`
-           #BID=`echo $DIR | rev | cut -d '/' -f2 | rev`
-           DIR=`echo $FNUM | awk -F";" {'print $6'}`
-           BID=`echo $DIR  | awk -F"/" {'print $7'}`
-           DLR=`echo $DIR  | sed "s/$BID\///"`
-           DOW1=$URL$DIR$ARQ1
+	  if [[ $FNUM != "" ]]; then
+	   #DIR=`echo $FNUM | cut -d ";" -f6`
+	   #BID=`echo $DIR | rev | cut -d '/' -f2 | rev`
+	   DIR=`echo $FNUM | awk -F";" {'print $6'}`
+	   BID=`echo $DIR  | awk -F"/" {'print $7'}`
+	   DLR=`echo $DIR  | sed "s/$BID\///"`
+	   DOW1=$URL$DIR$ARQ1
            DOW2=$URL$DIR$ARQ2
 
-           wget $DOW1 -P $RLOCAL$DLR
-           wget $DOW2 -P $RLOCAL$DLR
-           echo $FNUM >> $RLOCAL$CARD
-           echo $BID > $RLOCAL$DLR$BUIL
+	   wget $DOW1 -P $RLOCAL$DLR && echo $FNUM >> $RLOCAL$CARD
+           wget $DOW2 -P $RLOCAL$DLR && echo $BID > $RLOCAL$DLR$BUIL
+
+#REFRESH PAGE
+           echo $F5
 
 #DEBUG
 #echo $FNUM
@@ -88,10 +146,6 @@ echo "</form>"
 #echo $DOW2
 #echo $DOW1 -P $RLOCAL$DLR
 #echo $DOW2 -P $RLOCAL$DLR
-
-F5='<meta http-equiv="refresh" content="0;">'
-echo $F5
-
           fi
 
        done
