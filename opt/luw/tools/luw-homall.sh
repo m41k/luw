9b5b2c66a386b115d518357513b520d58254ee4a
m41k <maik.alberto@hotmail.com>
2017-08-01 14:59:38 -0300

    Update luw-homall.sh

--- opt/luw/tools/luw-homall.sh
+++ opt/luw/tools/luw-homall.sh
@@ -3,7 +3,7 @@
 #	 	      LUW-HOMALL - Luw.sh updates for all users                  #
 #               	CREATED BY: maik.alberto@hotmail.com                     #
 #--------------------------------------------------------------------------------#
-#!/bin/bash
+
 #cp /home/ubuntu/public_html/cgi-bin/luw.sh /opt/luw/luw.sh
 user=(${user[@]}`ls /home`)
 #echo ${user[1]}
